93591215500f638ff81d0667be303a2b7b1b4ac3
Time Kliss Solutions <48053167+klisssolutions@users.noreply.github.com>
2019-04-12 09:25:16 -0300

    Add files via upload

---
+++ script views.sql
@@ -0,0 +1,19 @@
+USE mydb;
+
+SELECT * FROM veiculo;
+SELECT * FROM modelo;
+SELECT * FROM marca;
+SELECT * FROM pendencia_veiculo;
+
+SELECT * FROM cliente;
+SELECT * FROM endereco;
+SELECT * FROM pendencia_cliente;
+
+CREATE VIEW VPendencia_Cliente AS SELECT p.idPendencia, c.nome, c.idCliente AS id, p.motivo, p.aberto FROM  pendencia_cliente AS p JOIN cliente AS c ON pc.idCliente = c.idCliente;
+CREATE VIEW VPendencia_Veiculo AS SELECT pv.idPendencia_Veiculo, CONCAT(mar.nomeMarca, " ", m.nomeModelo) AS nome, v.idVeiculo AS id, pv.motivo, pv.aberto FROM pendencia_veiculo AS pv JOIN veiculo AS v ON pv.idVeiculo = v.idVeiculo join modelo AS m ON m.idModelo = v.idModelo JOIN marca AS mar ON m.idMarca = mar.idMarca;
+
+SELECT * FROM VPendencia_Cliente;
+SELECT * FROM VPendencia_Veiculo;
+
+DROP VIEW VPendencia_Veiculo;
+DROP VIEW VPendencia_Cliente;
